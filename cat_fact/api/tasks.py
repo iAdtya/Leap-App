55e053a6e470178cf63a2beb355fec07fac9fa42
iAdtya <adityakhedekar98906@gmail.com>
2024-04-07 05:26:57 +0530

    Refactor fetch_cat_fact function to use async/await in tasks.py Update DEBUG setting to False in settings.py

--- cat_fact/api/tasks.py
+++ cat_fact/api/tasks.py
@@ -2,16 +2,19 @@ import requests
 from celery import Celery
 
 
-app = Celery('tasks', backend='rpc://', broker='pyamqp://')
+app = Celery("tasks", backend="rpc://", broker="pyamqp://")
+
 
 @app.task
-def fetch_cat_fact():
-  response = requests.get("https://cat-fact.herokuapp.com/facts")
-  if response.status_code == 200:
-    data = response.json()
-    if data:  # check if data is not empty
-      return {"fact": data[0]["text"]}  # return the "text" field of the first dictionary in the list
+async def fetch_cat_fact():
+    response = await requests.get("https://cat-fact.herokuapp.com/facts")
+    if response.status_code == 200:
+        data = response.json()
+        if data:  # check if data is not empty
+            return {
+                "fact": data[0]["text"]
+            }  # return the "text" field of the first dictionary in the list
+        else:
+            return {"error": "No facts found"}
     else:
-      return {"error": "No facts found"}
-  else:
-    return {"error": "Unable to fetch cat facts"}
+        return {"error": "no_task_has_been_queued_yet"}
